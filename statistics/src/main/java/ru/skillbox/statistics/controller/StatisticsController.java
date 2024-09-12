cd4a646883ff51825f5180274e8f2f400fea8d7d
Marat Murtuzaliev <marat.murtuzaliev@gmail.com>
2024-09-12 10:44:52 +0300

    fix: change ClassPathResource to FileUrlResource

--- statistics/src/main/java/ru/skillbox/statistics/controller/StatisticsController.java
+++ statistics/src/main/java/ru/skillbox/statistics/controller/StatisticsController.java
@@ -2,7 +2,7 @@ package ru.skillbox.statistics.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileUrlResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skillbox.statistics.service.StatisticsService;
+
+import java.net.MalformedURLException;
 
 @RestController
 @RequestMapping("/api/statistics")
@@ -24,12 +26,12 @@ public class StatisticsController {
     private String downloadFolder;
 
     @GetMapping("/download")
-    public ResponseEntity<Resource> download(@RequestParam String fileName) {
+    public ResponseEntity<Resource> download(@RequestParam String fileName) throws MalformedURLException {
         String filePath = downloadFolder + fileName;
 
         statisticsService.exportToCsv(filePath);
 
-        Resource fileResource = new ClassPathResource(filePath);
+        Resource fileResource = new FileUrlResource(filePath);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
